e5608e32dcfbf7e96268e42c63b2d35d8183025a
prabhav2002 <57176222+prabhav2002@users.noreply.github.com>
2022-06-12 14:33:34 +0530

    Add files via upload

---
+++ SprinklrDashApp5.4/index.py
@@ -0,0 +1,89 @@
+# importing libraries
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from app import app
+from apps import team, dev, home, export
+
+# the style arguments for the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+# contents of sidebar
+sidebar = html.Div(
+    [
+        html.Img(src="/assets/Sprinklr.png", height="50px"),
+        html.H2("Dashboard", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple dashboard to analyze unit testing of Sprinklr", className="lead"
+        ),
+        html.P("Timezone: Asia/Kolkata", className="lead"),
+        # redirection links to other pages of site
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Developer-wise", href="/dev", active="exact"),
+                dbc.NavLink("Team-wise", href="/team", active="exact"),
+                dbc.NavLink("Exports", href="/export", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+        html.Hr(),
+        html.A(
+            "Github", href="https://github.com/prabhav2002/UnitTestingDash-Sprinklr"
+        ),
+        html.Hr(),
+        html.A("Medium", href="https://medium.com/"),
+        html.Hr(),
+        html.A(
+            "Created by Prabhav Shah",
+            href="https://www.linkedin.com/in/prabhav-shah-7723281a0",
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+# callback for redirection
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return home.layout
+    elif pathname == "/dev":
+        return dev.layout
+    elif pathname == "/team":
+        return team.layout
+    elif pathname == "/export":
+        return export.layout
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ SprinklrDashApp5.4/teampage.py
@@ -0,0 +1,264 @@
+# importing libraries
+from elasticsearch import Elasticsearch
+from datetime import datetime as dt
+from datetime import date
+import pandas as pd
+import plotly.express as px
+from dateutil.relativedelta import relativedelta
+
+# connecting with elasticsearch server
+es = Elasticsearch()
+
+# function get total numbers of team and name of those teams
+def teampage_row1():
+    # aggregation to get all the teams
+    query_filter = {
+        "size": 0,
+        "aggs": {
+            "team": {"terms": {"field": "cloudName.keyword", "size": 2147483647}},
+        },
+    }
+    result = es.search(index="unit_test_tracker", body=query_filter)
+    teamSet = set()
+    for i in result["aggregations"]["team"]["buckets"]:
+        team = i["key"]
+        teamSet.add(team)
+    teamList = []
+    teamList = list(teamSet)
+    teamList.sort()
+    return len(teamSet), teamList
+
+
+# fucntion to plot pie-chart of team-wise developer count.
+def teampage_row2():
+    teams, teamList = teampage_row1()
+    data = []
+    # teamwise querying in for loop  to get the developer info of that team
+    for i in teamList:
+        query_filter = {
+            "from": 0,
+            "size": 10000,
+            "query": {
+                "term": {
+                    "cloudName.keyword": {
+                        "value": i,
+                    }
+                }
+            },
+            "aggs": {"dev": {"terms": {"field": "email.keyword", "size": 2147483647}}},
+        }
+        res = es.search(index="unit_test_tracker", body=query_filter)
+        for j in res["aggregations"]["dev"]["buckets"]:
+            devName = j["key"]
+            data.append([i, devName])
+
+    df = pd.DataFrame(data, columns=["Team", "Email"])
+    teamList.sort()
+    chartData = []
+    for i in teamList:
+        teamNameForChart = i
+        devCountPerTeamForChart = df["Team"].value_counts()[i]
+        chartData.append([teamNameForChart, devCountPerTeamForChart])
+    dfChart = pd.DataFrame(chartData, columns=["teamName", "devCountPerTeam"])
+    dfChart = dfChart.sort_values(by="teamName")
+    fig = px.pie(
+        dfChart,
+        values="devCountPerTeam",
+        names="teamName",
+        hole=0.3,
+        title="<b>Number of Developers per Team<b>",
+    )
+    return fig
+
+
+# function plot count of testcases vs time for selected teams for selected date-range
+# selection for date-wise, week-wise, month-wise aggregation
+# selection for type of testcases: added, deleted, effective
+def teampage_row3(timePeriod, teamNamesMultiDropdown, testCaseType, startdate, enddate):
+    if type(teamNamesMultiDropdown) == str:
+        teamNamesMultiDropdown = [teamNamesMultiDropdown]
+
+    # setting testcase-type
+    if testCaseType == "Effective Test Cases":
+        caseTypeString = "effectiveCount"
+    elif testCaseType == "Test Cases Added":
+        caseTypeString = "testAdded"
+    elif testCaseType == "Test Cases Deleted":
+        caseTypeString = "testDeleted"
+    data = []
+    # query to get count selected testcase type date-wise aggregated for selected teams
+    for i in teamNamesMultiDropdown:
+        query_filter = {
+            "from": 0,
+            "size": 10000,
+            "query": {
+                "term": {
+                    "cloudName.keyword": {
+                        "value": i,
+                    }
+                }
+            },
+            "aggs": {
+                "date": {
+                    "date_histogram": {
+                        "field": "fromTime",
+                        "interval": "day",
+                        "format": "yyyy-MM-dd",
+                    },
+                    "aggs": {"Total": {"sum": {"field": caseTypeString}}},
+                }
+            },
+        }
+        res = es.search(index="unit_test_tracker", body=query_filter)
+        for j in res["aggregations"]["date"]["buckets"]:
+            teamName = i
+            testCaseCount = j["Total"]["value"]
+            # converting timezone to Asia/Kolkata
+            timestamp = j["key"] + 66600000
+            date_time = dt.fromtimestamp(int(timestamp) / 1000)
+            data.append([date_time, testCaseCount, teamName])
+
+    # creating dataframe
+    df = pd.DataFrame(data, columns=["Date", "TestCaseCount", "Team"])
+
+    # filtering dataframe as per the date range chosen for analytics
+    start_date_object = date.fromisoformat(startdate)
+    start_date_string = start_date_object.strftime("%Y-%m-%d")
+    end_date_object = date.fromisoformat(enddate)
+    end_date_string = end_date_object.strftime("%Y-%m-%d")
+
+    df = df[(df["Date"] >= start_date_string) & (df["Date"] <= end_date_string)]
+    start_date_plus_month = start_date_object + relativedelta(months=1)
+
+    # aggregated date-wise and plotting of data
+    if timePeriod == "Date-wise Aggregation":
+        fig = px.bar(
+            df,
+            x="Date",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        # range slider option
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            range=[start_date_object, min(start_date_plus_month, end_date_object)],
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=7, label="1w", step="day", stepmode="backward"),
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+        )
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+    # aggregated week-wse and plotting of data
+    elif timePeriod == "Week-wise Aggregation":
+        dfWeek = df.copy()
+        dfWeek = (
+            dfWeek.groupby(["Team", pd.Grouper(key="Date", freq="W-SUN")])[
+                ("TestCaseCount")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfWeek["Date"] = dfWeek["Date"].dt.strftime("%W")
+        dfWeek.rename(columns={"Date": "Week"}, inplace=True)
+        fig = px.bar(
+            dfWeek,
+            x="Week",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        fig.update_xaxes(title="Week Number of the Year", rangeslider_visible=True)
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+    # aggregated month-wise and plotting of data
+    else:
+        dfMonth = df.copy()
+        dfMonth = (
+            dfMonth.groupby(["Team", pd.Grouper(key="Date", freq="1M")])[
+                ("TestCaseCount")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfMonth["Date"] = dfMonth["Date"].dt.strftime("%B")
+        dfMonth.rename(columns={"Date": "Month"}, inplace=True)
+        fig = px.bar(
+            dfMonth,
+            x="Month",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        fig.update_xaxes(title="Month", rangeslider_visible=True)
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+
+def teampage_row4(team, testCaseType):
+    # setting testcase-type
+    if testCaseType == "Effective Test Cases":
+        caseTypeString = "effectiveCount"
+    elif testCaseType == "Test Cases Added":
+        caseTypeString = "testAdded"
+    elif testCaseType == "Test Cases Deleted":
+        caseTypeString = "testDeleted"
+    teams, teamList = teampage_row1()
+    data = []
+    # query to get count of given testcase type developer wise for selected team
+    for i in teamList:
+        query_filter = {
+            "from": 0,
+            "size": 0,
+            "query": {
+                "term": {
+                    "cloudName.keyword": {
+                        "value": i,
+                    }
+                }
+            },
+            "aggs": {
+                "dev": {
+                    "terms": {"field": "email.keyword", "size": 2147483647},
+                    "aggs": {"testCaseCount": {"sum": {"field": caseTypeString}}},
+                },
+            },
+        }
+        res = es.search(index="unit_test_tracker", body=query_filter)
+        for j in res["aggregations"]["dev"]["buckets"]:
+            devName = j["key"]
+            count = j["testCaseCount"]["value"]
+            data.append([i, devName, count])
+
+    # creating dataframe
+    df = pd.DataFrame(data, columns=["Team", "Email", "Test Case Count"])
+    teamList.sort()
+    dfListTeamwise = {}
+    for i in teamList:
+        dfListTeamwise[i] = df[(df["Team"] == str(i))]
+        dfListTeamwise[i] = dfListTeamwise[i].sort_values("Email")
+    fig = px.bar(
+        dfListTeamwise[team],
+        x="Email",
+        y="Test Case Count",
+        title="Total " + testCaseType + " by Developers (Team: " + team + " )",
+        height=900,
+        width=1100,
+    )
+    return fig
