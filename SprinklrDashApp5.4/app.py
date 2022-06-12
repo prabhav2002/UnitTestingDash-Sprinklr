d83e5579a145e27d9193b3d09c5a601e4ff97c35
prabhav2002 <57176222+prabhav2002@users.noreply.github.com>
2022-06-12 14:32:16 +0530

    Delete SprinklrDashApp5.4 directory

--- SprinklrDashApp5.4/app.py
+++
@@ -1,8 +0,0 @@
-# importing libraries
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-server = app.server
-app.config.suppress_callback_exceptions = True
