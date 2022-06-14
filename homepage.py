cd45e48d2927131f1c0ecdbda09d4ff8f6ba3d06
prabhav2002 <57176222+prabhav2002@users.noreply.github.com>
2022-06-14 19:45:57 +0530

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,215 @@
+# importing libraries
+from elasticsearch import Elasticsearch
+from datetime import datetime as dt
+import pandas as pd
+import plotly.express as px
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+# connecting with elasticsearch server
+es = Elasticsearch()
+
+# function to get minimum and maximum date of the available data
+def homepage_row0():
+    # taking minimum and maximum from "fromTime" column consisting of timestamp as aggregation
+    query_filter = {
+        "size": 0,
+        "aggs": {
+            "min_date": {"min": {"field": "fromTime", "format": "yyyy-MM-dd"}},
+            "max_date": {"max": {"field": "fromTime", "format": "yyyy-MM-dd"}},
+        },
+    }
+    res = es.search(index="unit_test_tracker", body=query_filter)
+
+    # getting min_date and max_date from resultant timestamp, addition of +66600000 to change the timezone
+    min_date = dt.fromtimestamp(
+        int(res["aggregations"]["min_date"]["value"] + 66600000) / 1000
+    )
+    max_date = dt.fromtimestamp(
+        int(res["aggregations"]["max_date"]["value"] + 66600000) / 1000
+    )
+    return min_date, max_date
+
+
+# function to get total testcases added, total testcases deleted and total effective testcases in overall data
+def homepage_row1():
+    # counting overall testCasesAdded, testCasesDeleted, and totalEffectivetestCases by aggregation
+    query_filter = {
+        "size": 0,
+        "aggs": {
+            "total_added": {"sum": {"field": "testAdded"}},
+            "total_deleted": {"sum": {"field": "testDeleted"}},
+            "total_effective": {"sum": {"field": "effectiveCount"}},
+        },
+    }
+    result = es.search(index="unit_test_tracker", body=query_filter)
+
+    totalAdded = result["aggregations"]["total_added"]["value"]
+    totalDeleted = result["aggregations"]["total_deleted"]["value"]
+    totalEffective = result["aggregations"]["total_effective"]["value"]
+
+    return (totalAdded, totalDeleted, totalEffective)
+
+
+# function to get count of unique teams and unique developers in the provided data
+def homepage_row2():
+    # counting distinct number of teams and devs using cardinality as aggregation method
+    query_filter = {
+        "size": 0,
+        "aggs": {
+            "team": {"cardinality": {"field": "cloudName.keyword"}},
+            "dev": {"cardinality": {"field": "email.keyword"}},
+        },
+    }
+    result = es.search(index="unit_test_tracker", body=query_filter)
+
+    numberOfTeams = result["aggregations"]["team"]["value"]
+    numberOfDevs = result["aggregations"]["dev"]["value"]
+
+    return numberOfDevs, numberOfTeams
+
+
+# function to plot overall data analytics
+# with the feature of date-wise, week-wise, month-wise aggregation
+def homepage_row3(timePeriod, startdate, enddate):
+
+    # converting startdate, enddate to timestamp
+    start_date_object = date.fromisoformat(startdate)
+    end_date_object = date.fromisoformat(enddate)
+
+    # query to get the aggregation date-wise with total count of testcases added, deleted and effective.
+    query_filter = {
+        "size": 0,
+        # putting start-date and end-date range as filter
+        "query": {
+            "range": {
+                "fromTime": {
+                    "time_zone": "+05:30",
+                    "gte": start_date_object,
+                    "lte": end_date_object,
+                }
+            }
+        },
+        # summation of date-wise aggregation of every test case type
+        "aggs": {
+            "date": {
+                "date_histogram": {
+                    "field": "fromTime",
+                    "interval": "day",
+                    "format": "yyyy-MM-dd",
+                },
+                "aggs": {
+                    "DatewiseEffective": {"sum": {"field": "effectiveCount"}},
+                    "DatewiseAdded": {"sum": {"field": "testAdded"}},
+                    "DatewiseDeleted": {"sum": {"field": "testDeleted"}},
+                },
+            }
+        },
+    }
+    res = es.search(index="unit_test_tracker", body=query_filter)
+
+    # saving result to the data
+    data = []
+    for i in res["aggregations"]["date"]["buckets"]:
+        effective_count = i["DatewiseEffective"]["value"]
+        test_added = i["DatewiseAdded"]["value"]
+        test_deleted = i["DatewiseDeleted"]["value"]
+        # converting timezone to Asia/Kolkata
+        timestamp = i["key"] + 66600000
+        date_time = dt.fromtimestamp(int(timestamp) / 1000)
+        data.append([date_time, effective_count, test_added, test_deleted])
+
+    # creating dataframe
+    df = pd.DataFrame(
+        data,
+        columns=["Date", "TotalEffectiveTests", "TotalTestsAdded", "TotalTestsDeleted"],
+    )
+
+    # converting dataframe for week-wise aggregation and plotting of dataframe
+    if timePeriod == "Week-wise Aggregation":
+        dfWeek = df.copy()
+        dfWeek = (
+            dfWeek.groupby([pd.Grouper(key="Date", freq="W-SUN")])[
+                ("TotalEffectiveTests", "TotalTestsAdded", "TotalTestsDeleted")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfWeek["Date"] = dfWeek["Date"].dt.strftime("%W")
+        plot = px.bar(
+            dfWeek,
+            x="Date",
+            y=df.columns,
+            title="Week-wise Unit Testing Aggregation",
+            labels={"Date": "Week"},
+            height=700,
+        )
+        plot.update_xaxes(title="Week Number of the Year", rangeslider_visible=True)
+        plot.update_yaxes(title="Test Cases Count")
+        plot.update_layout(
+            legend=dict(
+                title="<b>Click here to<br>activate/deactivate<br>specific Test Case<br>Count Type<b>"
+            )
+        )
+        return plot
+
+    # converting dataframe for month-wise aggregation and plotting of dataframe
+    elif timePeriod == "Month-wise Aggregation":
+        dfMonth = df.copy()
+        dfMonth = (
+            dfMonth.groupby([pd.Grouper(key="Date", freq="1M")])[
+                ("TotalEffectiveTests", "TotalTestsAdded", "TotalTestsDeleted")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfMonth["Date"] = dfMonth["Date"].dt.strftime("%B")
+        plot = px.bar(
+            dfMonth,
+            x="Date",
+            y=df.columns,
+            title="Month-wise Unit Testing Aggregation",
+            labels={"Date": "Month"},
+            height=700,
+        )
+        plot.update_xaxes(title="Months", rangeslider_visible=True)
+        plot.update_yaxes(title="Test Cases Count")
+        plot.update_layout(
+            legend=dict(
+                title="<b>Click here to<br>activate/deactivate<br>specific Test Case<br>Count Type<b>"
+            )
+        )
+        return plot
+
+    # plotting date-wise
+    else:
+        plot = px.bar(
+            df,
+            x="Date",
+            y=df.columns,
+            title="Date-wise Unit Testing Aggregation",
+            height=700,
+        )
+        start_date_plus_month = start_date_object + relativedelta(months=1)
+        plot.update_xaxes(
+            rangeslider_visible=True,
+            range=[start_date_object, min(start_date_plus_month, end_date_object)],
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=7, label="1w", step="day", stepmode="backward"),
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+        )
+        plot.update_yaxes(title="Test Cases Count")
+        plot.update_layout(
+            legend=dict(
+                title="<b>Click here to<br>activate/deactivate<br>specific Test Case<br>Count Type<b>"
+            )
+        )
+        return plot
---
+++ teampage.py
@@ -0,0 +1,254 @@
+# importing libraries
+from elasticsearch import Elasticsearch
+from datetime import datetime as dt
+from datetime import date
+import pandas as pd
+import plotly.express as px
+from dateutil.relativedelta import relativedelta
+
+# connecting with elasticsearch server
+es = Elasticsearch()
+
+# function get total numbers of team and name of those teams
+def teampage_row1():
+    # aggregation to get all the teams
+    query_filter = {
+        "size": 0,
+        "aggs": {
+            "team": {"terms": {"field": "cloudName.keyword", "size": 2147483647}},
+        },
+    }
+    result = es.search(index="unit_test_tracker", body=query_filter)
+
+    # saving result to teamSet so that we will get all the unique teams
+    teamSet = set()
+    for i in result["aggregations"]["team"]["buckets"]:
+        team = i["key"]
+        teamSet.add(team)
+    teamList = list(teamSet)
+    teamList.sort()
+    return len(teamSet), teamList
+
+
+# fucntion to plot pie-chart of team-wise developer count.
+def teampage_row2():
+    teams, teamList = teampage_row1()
+    data = []
+    # teamwise querying in for loop to get the number of distinct developers of that team
+    for i in teamList:
+        query_filter = {
+            "size": 0,
+            "query": {
+                "term": {
+                    "cloudName.keyword": {
+                        "value": i,
+                    }
+                }
+            },
+            "aggs": {"dev": {"cardinality": {"field": "email.keyword"}}},
+        }
+        res = es.search(index="unit_test_tracker", body=query_filter)
+        # saving result to data list
+        data.append([i, res["aggregations"]["dev"]["value"]])
+
+    dfChart = pd.DataFrame(data, columns=["teamName", "devCountPerTeam"])
+    dfChart = dfChart.sort_values(by="teamName")
+    fig = px.pie(
+        dfChart,
+        values="devCountPerTeam",
+        names="teamName",
+        hole=0.3,
+        title="<b>Number of Developers per Team<b>",
+    )
+    return fig
+
+
+# function plot count of testcases vs time for selected teams for selected date-range
+# selection for date-wise, week-wise, month-wise aggregation
+# selection for type of testcases: added, deleted, effective
+def teampage_row3(timePeriod, teamNamesMultiDropdown, testCaseType, startdate, enddate):
+    if type(teamNamesMultiDropdown) == str:
+        teamNamesMultiDropdown = [teamNamesMultiDropdown]
+
+    # setting testcase-type
+    if testCaseType == "Effective Test Cases":
+        caseTypeString = "effectiveCount"
+    elif testCaseType == "Test Cases Added":
+        caseTypeString = "testAdded"
+    elif testCaseType == "Test Cases Deleted":
+        caseTypeString = "testDeleted"
+
+    data = []
+    # query to get count selected testcase type date-wise aggregated for selected teams
+    for i in teamNamesMultiDropdown:
+        query_filter = {
+            "size": 0,
+            "query": {
+                "term": {
+                    "cloudName.keyword": {
+                        "value": i,
+                    }
+                },
+            },
+            "aggs": {
+                "date": {
+                    "date_histogram": {
+                        "field": "fromTime",
+                        "interval": "day",
+                        "format": "yyyy-MM-dd",
+                    },
+                    "aggs": {"Total": {"sum": {"field": caseTypeString}}},
+                }
+            },
+        }
+        res = es.search(index="unit_test_tracker", body=query_filter)
+        for j in res["aggregations"]["date"]["buckets"]:
+            teamName = i
+            testCaseCount = j["Total"]["value"]
+            # converting timezone to Asia/Kolkata
+            timestamp = j["key"] + 66600000
+            date_time = dt.fromtimestamp(int(timestamp) / 1000)
+            data.append([date_time, testCaseCount, teamName])
+
+    # creating dataframe
+    df = pd.DataFrame(data, columns=["Date", "TestCaseCount", "Team"])
+
+    # filtering dataframe as per the date range chosen for analytics
+    start_date_object = date.fromisoformat(startdate)
+    start_date_string = start_date_object.strftime("%Y-%m-%d")
+    end_date_object = date.fromisoformat(enddate)
+    end_date_string = end_date_object.strftime("%Y-%m-%d")
+
+    df = df[(df["Date"] >= start_date_string) & (df["Date"] <= end_date_string)]
+    start_date_plus_month = start_date_object + relativedelta(months=1)
+
+    # aggregated date-wise and plotting of data
+    if timePeriod == "Date-wise Aggregation":
+        fig = px.bar(
+            df,
+            x="Date",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        # range slider option
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            range=[start_date_object, min(start_date_plus_month, end_date_object)],
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=7, label="1w", step="day", stepmode="backward"),
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+        )
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+    # aggregated week-wse and plotting of data
+    elif timePeriod == "Week-wise Aggregation":
+        dfWeek = df.copy()
+        dfWeek = (
+            dfWeek.groupby(["Team", pd.Grouper(key="Date", freq="W-SUN")])[
+                ("TestCaseCount")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfWeek["Date"] = dfWeek["Date"].dt.strftime("%W")
+        dfWeek.rename(columns={"Date": "Week"}, inplace=True)
+        fig = px.bar(
+            dfWeek,
+            x="Week",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        fig.update_xaxes(title="Week Number of the Year", rangeslider_visible=True)
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+    # aggregated month-wise and plotting of data
+    else:
+        dfMonth = df.copy()
+        dfMonth = (
+            dfMonth.groupby(["Team", pd.Grouper(key="Date", freq="1M")])[
+                ("TestCaseCount")
+            ]
+            .sum()
+            .reset_index()
+            .sort_values("Date")
+        )
+        dfMonth["Date"] = dfMonth["Date"].dt.strftime("%B")
+        dfMonth.rename(columns={"Date": "Month"}, inplace=True)
+        fig = px.bar(
+            dfMonth,
+            x="Month",
+            y="TestCaseCount",
+            color="Team",
+            title="Team-wise " + testCaseType,
+            height=900,
+            width=1100,
+        )
+        fig.update_xaxes(title="Month", rangeslider_visible=True)
+        fig.update_yaxes(title=testCaseType)
+        return fig
+
+
+def teampage_row4(team, testCaseType):
+    # setting testcase-type
+    if testCaseType == "Effective Test Cases":
+        caseTypeString = "effectiveCount"
+    elif testCaseType == "Test Cases Added":
+        caseTypeString = "testAdded"
+    elif testCaseType == "Test Cases Deleted":
+        caseTypeString = "testDeleted"
+
+    data = []
+    # query to get count of given testcase type developer wise for selected team
+    query_filter = {
+        "size": 0,
+        "query": {
+            "term": {
+                "cloudName.keyword": {
+                    "value": team,
+                }
+            }
+        },
+        # aggregation to find count of selected type of test cases of every developer of the team
+        "aggs": {
+            "dev": {
+                "terms": {"field": "email.keyword", "size": 2147483647},
+                "aggs": {"testCaseCount": {"sum": {"field": caseTypeString}}},
+            },
+        },
+    }
+    res = es.search(index="unit_test_tracker", body=query_filter)
+
+    # saving result to data
+    for j in res["aggregations"]["dev"]["buckets"]:
+        devName = j["key"]
+        count = j["testCaseCount"]["value"]
+        data.append([devName, count])
+
+    # creating dataframe and sorting it by Email field
+    df = pd.DataFrame(data, columns=["Email", "Test Case Count"])
+    df = df.sort_values("Email")
+
+    fig = px.bar(
+        df,
+        x="Email",
+        y="Test Case Count",
+        title="Total " + testCaseType + " by Developers (Team: " + team + " )",
+        height=900,
+        width=1100,
+    )
+    return fig
